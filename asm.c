5c8cc5ec217f670ec2fed730ab5eab88c89eb216
1318781143@qq.com <1318781143@qq.com>
2022-07-21 23:31:18 +0800

    finish backtrace

---
+++ asm.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main (){
+
+unsigned int i = 0x00646c72;
+printf("H%x Wo%s \n", 57616, &i);
+printf("x=%d y=%d", 3);
+}
+
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -62,13 +62,16 @@ sys_sleep(void)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
+
   while(ticks - ticks0 < n){
     if(myproc()->killed){
       release(&tickslock);
       return -1;
     }
+     backtrace();
     sleep(&ticks, &tickslock);
   }
+
   release(&tickslock);
   return 0;
 }
